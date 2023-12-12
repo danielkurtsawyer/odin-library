11fed89295b6d2e72d5fa5729441430efa7c1732
danielkurtsawyer <19danielsawyer@gmail.com>
2023-12-12 14:37:40 -0500

    Implement custom JavaScript validation and error messages

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const myLibrary = [];
 const bookButton = document.querySelector(".book-button");
+
 const bookForm = document.querySelector("#new-book-dialog");
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+const titleError = document.querySelector('.title-error');
+const authorInput = document.querySelector('#author');
+const authorError = document.querySelector('.author-error');
+const pagesInput = document.querySelector('#pages');
+const pagesError = document.querySelector('.pages-error');
+
 const readBox = document.querySelector("#read");
 const library = document.querySelector(".library");
 
@@ -97,18 +106,98 @@ function displayBooks(){
     });
 }
 
+function showTitleError(){
+    if(titleInput.validity.valueMissing){
+        titleError.textContent = "You need to enter a title";
+    } else if(titleInput.validity.tooShort){
+        titleError.textContent = `Title length must be at least ${titleInput.minLength} characters`;
+    } else if(titleInput.validity.tooLong){
+        titleError.textContent = `Title length must be at most ${titleInput.maxLength} characters`;
+    }
+
+    titleError.classList.add('active');
+}
+
+function showAuthorError(){
+    if(authorInput.validity.valueMissing){
+        authorError.textContent = "You need to enter an author name";
+    } else if(authorInput.validity.tooShort){
+        authorError.textContent = `Author name length must be at least ${authorInput.minLength} characters`;
+    } else if(authorInput.validity.tooLong){
+        authorError.textContent = `Title length must be at most ${authorInput.maxLength} characters`;
+    }
+
+    authorError.classList.add('active');
+}
+
+function showPagesError(){
+    if(pagesInput.validity.valueMissing){
+        pagesError.textContent = "You need to enter a page count";
+    } else if(pagesInput.validity.rangeUnderflow){
+        pagesError.textContent = `A book must have at least ${pagesInput.min} page`;
+    }
+
+    pagesError.classList.add('active');
+}
+
+function showError(){
+    if(!titleInput.validity.valid){
+        showTitleError();
+    }
+    if(!authorInput.validity.valid){
+        showAuthorError();
+    }
+    if(!pagesInput.validity.valid){
+        showPagesError();
+    }
+}
+
 bookButton.addEventListener("click", ()=>{bookForm.showModal()});
 readBox.addEventListener("change", (e)=>{
     readBox.value = readBox.value === "0" ? "1" : "0";
 })
+
+titleInput.addEventListener('input', (e) => {
+    if(titleInput.validity.valid){
+        titleError.textContent = "";
+        titleError.classList.remove('active');
+    } else {
+        showTitleError();
+    }
+})
+
+authorInput.addEventListener('input', (e) => {
+    if(authorInput.validity.valid){
+        authorError.textContent = "";
+        authorError.classList.remove('active');
+    } else {
+        showAuthorError();
+    }
+})
+
+pagesInput.addEventListener('input', (e) => {
+    if(pagesInput.validity.valid){
+        pagesError.textContent = "";
+        pagesError.classList.remove('active');
+    } else{
+        showPagesError();
+    }
+})
+
 bookForm.addEventListener("submit", (e)=>{
+    console.log(form.checkValidity());
     e.preventDefault();
-    const title = e.target.title.value;
-    const author = e.target.author.value;
-    const pages = e.target.pages.value;
-    const read = e.target.read.value;
-    const tempBook = new Book(title, author, pages, read);
-    addBookToLibrary(tempBook);
-    displayBooks();
-    bookForm.close();
+    
+    if(!form.checkValidity()){
+        showError();
+    } else {
+        const title = e.target.title.value;
+        const author = e.target.author.value;
+        const pages = e.target.pages.value;
+        const read = e.target.read.value;
+        const tempBook = new Book(title, author, pages, read);
+        addBookToLibrary(tempBook);
+        displayBooks();
+        bookForm.close();
+    }
 })
